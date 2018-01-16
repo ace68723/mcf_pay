612012668b159ee207f6fb7ae34bfa83840d04de
xunrui-chanmao <xunrui@chanmao.ca>
2018-01-16 10:30:38 +0800

    a better way to fix the previous bug, as there are multiple null attributes

--- app/Providers/RttService/RttService.php
+++ app/Providers/RttService/RttService.php
@@ -422,10 +422,20 @@ class RttService{
         if (count($new_recs) == 0) return;
         Log::DEBUG('compare result: new:'.json_encode(array_pluck($new_recs,'ref_id')));
         try {
-            DB::table('txn_base')->insert($new_recs);
+            if (count($new_recs) == 1) {
+                foreach($new_recs as $k=>$v) {
+                    if (is_null($v))
+                        unset($new_recs[$k]);
+                }
+                DB::table('txn_base')->insert($new_recs[0]);
+            }
+            else {
+                DB::table('txn_base')->insert($new_recs);
+            }
         }
         catch (\PDOException $e) {
             if (23000 != $e->getCode()) throw $e;
+            echo $e->getMessage();
             throw new RttException('SYSTEM_ERROR',
                 'insert violate unique constraint. Maybe caused by refund txn time diff. Try another TimeWindow');
         }
