9a0381894cb9851623b46b4907cd1981a88782b9
xunrui-chanmao <xunrui@chanmao.ca>
2018-01-18 03:52:47 +0800

    add schedule downloading and compare wx_raw_bills and txn_base

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Log;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
@@ -26,11 +27,20 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // note that this function will be executed every time artisan is called, independent of which command
-            /*
+        //Log::DEBUG('setting schedule func called');
         $schedule->call(function () {
-            $sp = app()->make('wx_service');
+            Log::DEBUG('running scheduled_wx_sync');
+            $sp = app()->make('wx_vendor_service');
             $sp->sync_bill();
-        })->everyDay();
-             */
+        })->dailyAt('04:00');
+        $schedule->call(function () {
+            Log::DEBUG('running scheduled_compare');
+            $rtt_sp = app()->make('rtt_service');
+            $start = new \DateTime("now");
+            $end = new \DateTime("now");
+            $start->modify("-4 day");
+            $end->modify("-1 day");
+            $rtt_sp->compare("wx",$start->getTimestamp(),$end->getTimestamp());
+        })->dailyAt('05:00');
     }
 }
